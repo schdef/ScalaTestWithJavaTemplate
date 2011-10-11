d8015d212b04fb6350cf6fdbf969453ed348474d
schdef <stefanheintz+coding@gmail.com>
2011-10-11 22:28:07 +0200

    java extensions

---
+++ src/main/java/de/schdef/slash/coding/dummy/DataUtil.java
@@ -0,0 +1,11 @@
+package de.schdef.slash.coding.dummy;
+
+public class DataUtil {
+
+	public DataUtil(){
+	}
+	
+	public boolean isValid(Number value) {
+		return false;
+	}
+}
---
+++ src/test/scala/de/schdef/slash/coding/template/DataUtilScalaTest.scala
@@ -0,0 +1,18 @@
+package de.schdef.slash.coding.template
+
+import org.specs2.mutable._
+import org.junit.runner._
+import org.specs2.runner._
+import de.schdef.slash.coding.dummy._
+
+@RunWith(classOf[JUnitRunner])
+class DataUtilScalaTest extends Specification {
+
+  "The given value" should {
+    "be invalid" in {
+      var util = new DataUtil()
+      util.isValid(12L) must be equalTo(false)
+    }
+  }
+  
+}
